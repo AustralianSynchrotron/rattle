53f3bf7ba217900e1a224fb010124adfaff29fab
Andrew Starritt <andrew.starritt@gmail.com>
2026-01-17 18:07:24 +1100

    Use SPDX-License-Identifier notation.

--- RattleSup/Rattle.c
+++ RattleSup/Rattle.c
@@ -1,6 +1,9 @@
 /* File: RattleSup/Rattle.c
- * DateTime: Thu Sep 25 15:12:44 2025
+ * DateTime: Sat Jan 17 17:52:41 2026
  * Last checked in by: starritt
+ *
+ * SPDX-FileCopyrightText: 2025-2026 Australian Synchrotron
+ * SPDX-License-Identifier: LGPL-2.1-only
  *
  * Description
  * rattle - Rate And Time To Limit Estimate
@@ -73,22 +76,6 @@
  * OUTH - DOUBLE  - rate of change (based on INPH elements)                 - (EGU/s)*INPS.
  * OUTI - DOUBLE  - estimated time to reach limit/threshold (based on INPI) - secs/INPT.
  *
- *
- * Copyright (c) 2025 Australian Synchrotron
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * Licence as published by the Free Software Foundation; either
- * version 2.1 of the Licence, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public Licence for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * Licence along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  * Original author: Andrew Starritt
  * Maintained by:   Andrew Starritt
